06f4266e7bc35390a39ff8f25a86fc23528c2099
Ideflop <94184575+Ideflop@users.noreply.github.com>
2022-03-28 09:28:27 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -9,8 +9,9 @@ class Sort():
     def __init__(self):
         self.today = today = datetime.date.today()
         #self.yesterday = self.today - datetime.timedelta(days = 1)
-        self.yesterday = '2022-03-21'
+        self.yesterday = '2022-03-24'
         self.file_created = ['Page','Vehicle_journey','Routes']
+        self.NULL = 'NULL'
 
     def how_many_files(self,i):        
         count_file = 0
@@ -21,11 +22,10 @@ class Sort():
 
         return count_file
 
-    def wich_data(self,z):
+    def which_data(self,z):
         with open(f'{self.yesterday}/Page/Page{z}.txt') as json_file:
-            data = json.load(json_file)
+            return json.load(json_file)
 
-            return data
         
     def disruptions(self,z,i,j):
 
@@ -38,91 +38,91 @@ class Sort():
             try:    
                 no, begin = str(data["disruptions"][i]['application_periods'][0]['begin']).split('T') # give just hhmmss
             except:
-                begin = None
+                begin = NULL
             try:    
                 no, end = str(data["disruptions"][i]['application_periods'][0]['end']).split('T') # give just hhmmss
             except:
-                end = None
+                end = self.NULL
             try:    
                 id = data["disruptions"][i]['id'] # = disruption_id disruption_uri impacteed_object
             except:
-                id = None
+                id = self.NULL
             try:
                 message = data["disruptions"][i]['messages'][0]['text'] # try
             except:
-                message = None
+                message = self.NULL
             try:    
                 severity_effect = data["disruptions"][i]['severity']['effect']
             except:
-                severity_effect = None
+                severity_effect = self.NULL
             try:    
                 severity_name = data["disruptions"][i]['severity']['name']
             except:
-                severity_name = None
+                severity_name = self.NULL
             try:    
                 trip_id = data["disruptions"][i]['impacted_objects'][0]['pt_object']['trip']['id']
             except:
-                trip_id = None
+                trip_id = self.NULL
             try:    
                 trip_name = data["disruptions"][i]['impacted_objects'][0]['pt_object']['trip']['name']
             except:
-                trip_name = None
+                trip_name = self.NULL
             try:
                 amended_arrival_time = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['amended_arrival_time']
             except:
-                amended_arrival_time = None
+                amended_arrival_time = self.NULL
             try:
                 amended_departure_time = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['amended_departure_time']
             except:
-                amended_departure_time = None
+                amended_departure_time = self.NULL
             try:    
                 departure_status = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['departure_status']
             except:
-                departure_status = None
+                departure_status = self.NULL
             try:    
                 base_arrival_time = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['base_arrival_time']
             except:
-                base_arrival_time = None
+                base_arrival_time = self.NULL
             try:    
                 base_departure_time = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['base_departure_time']
             except:
-                base_departure_time = None
+                base_departure_time = self.NULL
             try:    
                 arrival_status = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['arrival_status']
             except:
-                arrival_status = None
+                arrival_status = self.NULL
             try:    
                 cause = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['cause']
             except:
-                cause = None
+                cause = self.NULL
             try:    
                 is_detour = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['is_detour']
             except:
-                is_detour = None
+                is_detour = self.NULL
             try:    
                 lat = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_point']['coord']['lat']
             except:
-                lat = None
+                lat = self.NULL
             try:    
                 lon = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_point']['coord']['lon']
             except:
-                lon = None
+                lon = self.NULL
             try:    
                 id_impacted_stop = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_point']['id']
             except:
-                id_impacted_stop = None
+                id_impacted_stop = self.NULL
             try:    
                 label_impacted_stop = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_point']['label']
             except:
-                label_impacted_stop = None
+                label_impacted_stop = self.NULL
             try:    
                 name_impacted_stop = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_point']['name']
             except:
-                name_impacted_stop = None
+                name_impacted_stop = self.NULL
             try:    
                 stop_time_effect = data["disruptions"][i]['impacted_objects'][j]['impacted_stops'][0]['stop_time_effect']
             except:
-                stop_time_effect = None
+                stop_time_effect = self.NULL
 
             return [date, begin, end, id, message, severity_effect, severity_name, trip_id, trip_name],[ date, begin, end, value, amended_arrival_time, amended_departure_time, departure_status, base_arrival_time, base_departure_time, arrival_status, cause, is_detour, lat, lon, id_impacted_stop, label_impacted_stop, name_impacted_stop, stop_time_effect]
 
@@ -138,80 +138,80 @@ class Sort():
             try:    
                 begin = data2['vehicle_journeys'][i]['calendars'][0]['active_periods'][0]['begin']
             except:
-                begin = None
+                begin = self.NULL
             try:    
                 end = data2['vehicle_journeys'][i]['calendars'][0]['active_periods'][0]['end']
             except:
-                end = None
+                end = self.NULL
             # week_pattern = data2['vehicle_journeys'][i]['calendars'][0]['week_pattern'] # garde just true
             try:    
                 id = data2['vehicle_journeys'][i]['id']
             except:
-                id = None
+                id = self.NULL
             try:    
                 name = data2['vehicle_journeys'][i]['name'] # = headsign
             except:
-                name = None
+                name = self.NULL
             try:    
                 trip_id = data2['vehicle_journeys'][i]['trip']['id']
             except:
-                trip_id = None
+                trip_id = self.NULL
             try:    
                 trip_name = data2['vehicle_journeys'][i]['trip']['name']
             except:
-                trip_name = None
+                trip_name = self.NULL
             try:    
                 arrival_time = data2['vehicle_journeys'][i]['stop_times'][j]['arrival_time']
             except:
-                arrival_time = None
+                arrival_time = self.NULL
             try:    
                 departure_time = data2['vehicle_journeys'][i]['stop_times'][j]['departure_time']
             except:
-                departure_time = None
+                departure_time = self.NULL
             try:    
                 drop_off_allowed = data2['vehicle_journeys'][i]['stop_times'][j]['drop_off_allowed']
             except:
-                drop_off_allowed = None
+                drop_off_allowed = self.NULL
             try:    
                 headsign = data2['vehicle_journeys'][i]['stop_times'][j]['headsign']
             except:
-                headsign = None
+                headsign = self.NULL
             try:    
                 pickup_allowed = data2['vehicle_journeys'][i]['stop_times'][j]['pickup_allowed']
             except:
-                pickup_allowed = None
+                pickup_allowed = self.NULL
             try:    
                 skipped_stop = data2['vehicle_journeys'][i]['stop_times'][j]['skipped_stop']
             except:
-                skipped_stop = None
+                skipped_stop = self.NULL
             try:    
                 lat = data2['vehicle_journeys'][i]['stop_times'][j]['stop_point']['coord']['lat']
             except:
-                lat = None
+                lat = self.NULL
             try:    
                 lon = data2['vehicle_journeys'][i]['stop_times'][j]['stop_point']['coord']['lon']
             except:
-                lon = None
+                lon = self.NULL
             try:    
                 id_stop_point = data2['vehicle_journeys'][i]['stop_times'][j]['stop_point']['id']
             except:
-                id_stop_point = None
+                id_stop_point = self.NULL
             try:    
                 label_stop_point = data2['vehicle_journeys'][i]['stop_times'][j]['stop_point']['label']
             except:
-                label_stop_point = None
+                label_stop_point = self.NULL
             try:    
                 name_stop_point = data2['vehicle_journeys'][i]['stop_times'][j]['stop_point']['name']
             except:
-                name_stop_point = None
+                name_stop_point = self.NULL
             try:    
                 utc_arrival_time = data2['vehicle_journeys'][i]['stop_times'][j]['utc_arrival_time']
             except:
-                utc_arrival_time = None
+                utc_arrival_time = self.NULL
             try:
                 utc_departure_time = data2['vehicle_journeys'][i]['stop_times'][j]['utc_departure_time']
             except:
-                utc_departure_time = None
+                utc_departure_time = self.NULL
                 
             return [date, begin, end, id, name, trip_id, trip_name],[date, begin, end, value, arrival_time, departure_time, drop_off_allowed, headsign, pickup_allowed, skipped_stop, lat, lon, id_stop_point, label_stop_point, name_stop_point, utc_arrival_time, utc_departure_time]
 
@@ -227,91 +227,91 @@ class Sort():
             try:
                 embedded_type = data3['routes'][i]['direction']['embedded_type']
             except:
-                embedded_type = None
+                embedded_type = self.NULL
             try:
                 id_direction = data3['routes'][i]['direction']['id']
             except:
-                id_direction = None
+                id_direction = self.NULL
             try:
                 name_direction = data3['routes'][i]['direction']['name']
             except:
-                name_direction = None
+                name_direction = self.NULL
             try:
                 quality = data3['routes'][i]['direction']['quality'] # utile ?
             except:
-                quality = None
+                quality = self.NULL
             try:
                 value = data3['routes'][i]['direction']['stop_area']['codes'][0]['value']
             except:
-                value = None
+                value = self.NULL
             try:
                 lat = data3['routes'][i]['direction']['stop_area']['coord']['lat']
             except:
-                lat = None
+                lat = self.NULL
             try:
                 lon = data3['routes'][i]['direction']['stop_area']['coord']['lon']
             except:
-                lon = None
+                lon = self.NULL
             try:
                 id_stop_area = data3['routes'][i]['direction']['stop_area']['id']
             except:
-                id_stop_area = None
+                id_stop_area = self.NULL
             try:
                 label_stop_area = data3['routes'][i]['direction']['stop_area']['label']
             except:
-                label_stop_area = None
+                label_stop_area = self.NULL
             try:
                 name_stop_area = data3['routes'][i]['direction']['stop_area']['name']
             except:
-                name_stop_area = None
+                name_stop_area = self.NULL
             try:
                 direction_type = data3['routes'][i]['direction_type']
             except:
-                direction_type = None
+                direction_type = self.NULL
             try:
                 id = data3['routes'][i]['id']
             except:
-                id = None
+                id = self.NULL
             try:
                 is_frequence = data3['routes'][i]['is_frequence']
             except:
-                is_frequence = None
+                is_frequence = self.NULL
             try:
                 closing_time = data3['routes'][i]['line']['closing_time']
             except:
-                closing_time = None
+                closing_time = self.NULL
             try:
                 id_commercial_mode = data3['routes'][i]['line']['commercial_mode']['id']
             except:
-                id_commercial_mode = None
+                id_commercial_mode = self.NULL
             try:
                 name_commercial_mode = data3['routes'][i]['line']['commercial_mode']['name']
             except:
-                name_commercial_mode = None
+                name_commercial_mode = self.NULL
             try:
                 id_line = data3['routes'][i]['line']['id']
             except:
-                id_line = None
+                id_line = self.NULL
             try:
                 id_name = data3['routes'][i]['line']['name']
             except:
-                id_name = None
+                id_name = self.NULL
             try:
                 opening_time = data3['routes'][i]['line']['opening_time']
             except:
-                opening_time = None
+                opening_time = self.NULL
             try:
                 id_physical_modes = data3['routes'][i]['line']['physical_modes'][0]['id']
             except:
-                id_physical_modes = None
+                id_physical_modes = self.NULL
             try:
                 name_physical_modes = data3['routes'][i]['line']['physical_modes'][0]['name']
             except:
-                name_physical_modes = None
+                name_physical_modes = self.NULL
             try:
                 name = data3['routes'][i]['name']
             except:
-                name = None
+                name = self.NULL
                     
 
             return [date, embedded_type, id_direction, name_direction, quality, value, lat, lon, id_stop_area, label_stop_area, name_stop_area, direction_type, id, is_frequence, closing_time, id_commercial_mode, name_commercial_mode, id_line, id_name, opening_time, id_physical_modes, name_physical_modes, name]
