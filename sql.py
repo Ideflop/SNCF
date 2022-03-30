ed639fca6d71cfdaefd0e88a0c967c885ae0d598
Ideflop <94184575+Ideflop@users.noreply.github.com>
2022-03-30 23:06:12 +0200

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python3
+
 import mysql.connector 
 import extract
 
-mydb = mysql.connector.connect(
+mydb = mysql.connector.connect( # connect to database
     host="127.0.0.1",
     user="root",
     database="SNCF"
@@ -14,7 +16,7 @@ class Create():
     def __init__(self):
         self.mycursor = mydb.cursor()
         
-    def table(self):
+    def table(self): # create sql table if not exist 
 
         try:
             sql = "CREATE TABLE disruptions (date DATE, begin TIME, end TIME, id VARCHAR(255), message TEXT, severity_effect VARCHAR(255), severity_name VARCHAR(255), trip_id VARCHAR(255) , trip_name INT)"
@@ -37,8 +39,8 @@ class Create():
         except:
             pass
 
-        try:
-            sql = "CREATE TABLE routes (date DATE, embedded_type VARCHAR(255), direction_id VARCHAR(255), start_name VARCHAR(255), quality INT, stop_area INT, lat FLOAT, lon FLOAT, label VARCHAR(255), name VARCHAR(255), direction_type VARCHAR(255), id VARCHAR(255), is_frequence VARCHAR(255), closing_time TIME, commercial_mode_id VARCHAR(255), commercial_mode_name VARCHAR(255), line_id VARCHAR(255), line_name VARCHAR(255), opening_time TIME, physical_modes_id VARCHAR(255), stop_name VARCHAR(255), routes_name VARCHAR(255))"
+        try:                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+            sql = "CREATE TABLE routes (date DATE, embedded_type VARCHAR(255), id_direction VARCHAR(255), name_direction VARCHAR(255), quality INT, value TEXT, id_stop_area TEXT, lat FLOAT, lon FLOAT, label_stop_area VARCHAR(255), name_stop_area VARCHAR(255), direction_type VARCHAR(255), id VARCHAR(255), is_frequence VARCHAR(255), closing_time TIME, id_commercial_mode VARCHAR(255), name_commercial_mode VARCHAR(255), id_line VARCHAR(255), id_name VARCHAR(255), opening_time TIME, id_physical_modes VARCHAR(255), name_physical_modes VARCHAR(255), routes_name VARCHAR(255))"
             self.mycursor.execute(sql)
         except:
             pass
@@ -54,47 +56,48 @@ class Insert():
         self.four = ['date', 'begin', 'end', 'value', 'arrival_time', 'departure_time', 'drop_off_allowed', 'headsign', 'pickup_allowed', 'skipped_stop', 'lat', 'lon', 'id_stop_point', 'label_stop_point', 'name_stop_point', 'utc_arrival_time', 'utc_departure_time']
 
 
-    def disruptions(self):
+    def disruptions(self): # insert disruptions
         result = extract.Sort().how_many_files(0)
         
-        for z in range(result-1):
+        for z in range(result):
             i = 0
-            data = extract.Sort().which_data(z)
+            data = extract.Sort().which_data(z,1)
             for x in data['disruptions']:
-                j = 0
+
                 for y in data['disruptions'][i]['impacted_objects']:
+                    j=0
                     disruption, impacted_object = extract.Sort().disruptions(z,i,j)
-                    for a in range(len(self.one)):
-                        if self.one[a] == 'date':
-                            sql = f"INSERT INTO disruptions (date) VALUES (str_to_date('{disruption[a]}','%Y-%m-%d'))"
-                        elif disruption[a] == 'NULL':
-                            sql = f"INSERT INTO disruptions ({self.one[a]}) VALUES ({disruption[a]})"
-                        else:
-                            sql = f'INSERT INTO disruptions ({self.one[a]}) VALUES ("{disruption[a]}")'
-                        #print(self.one[a])
-                        self.mycursor.execute(sql)
-                        mydb.commit()
-                    for b in range(len(self.two)):
-                        if self.two[b] == 'date':
-                            sql = f"INSERT INTO impacted_object ({self.two[b]}) VALUES (str_to_date('{impacted_object[b]}','%Y-%m-%d'))"
-                        elif impacted_object[b] == 'NULL':
-                            sql = f"INSERT INTO impacted_object ({self.two[b]}) VALUES ({impacted_object[b]})"
-                        else:
-                            sql = f'INSERT INTO impacted_object ({self.two[b]}) VALUES ("{impacted_object[b]}")'
-                        #print(self.two[b])
-                        self.mycursor.execute(sql)
-                        mydb.commit()
-                    j += 1
-                    print(j)
+
+                    sql = "INSERT INTO disruptions (date, begin, end, id, message, severity_effect, severity_name, trip_id, trip_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                    val = (disruption[0], disruption[1], disruption[2], disruption[3], disruption[4], disruption[5], disruption[6], disruption[7], disruption[8])
+                    self.mycursor.execute(sql,val)
+                    mydb.commit()
+                    
+                    sql = "INSERT INTO impacted_object (date, begin, end, value, amended_arrival_time, amended_departure_time, departure_status, base_arrival_time, base_departure_time, arrival_status, cause, is_detour, lat, lon, id_impacted_stop, label_impacted_stop, name_impacted_stop, stop_time_effect) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                    val = (impacted_object[0], impacted_object[1], impacted_object[2], impacted_object[3], impacted_object[4], impacted_object[5], impacted_object[6], impacted_object[7], impacted_object[8], impacted_object[9], impacted_object[10], impacted_object[11], impacted_object[12], impacted_object[13], impacted_object[14], impacted_object[15], impacted_object[16], impacted_object[17])
+                    self.mycursor.execute(sql,val)
+                    mydb.commit()
+                    try:
+                        f = 1
+                        for w in data['disruptions'][i]['impacted_objects'][0]['impacted_stops'][f]:
+                            disruption, impacted_object = extract.Sort().disruptions(z,i,j)
+                            sql = "INSERT INTO impacted_object (date, begin, end, value, amended_arrival_time, amended_departure_time, departure_status, base_arrival_time, base_departure_time, arrival_status, cause, is_detour, lat, lon, id_impacted_stop, label_impacted_stop, name_impacted_stop, stop_time_effect) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                            val = (impacted_object[0], impacted_object[1], impacted_object[2], impacted_object[3], impacted_object[4], impacted_object[5], impacted_object[6], impacted_object[7], impacted_object[8], impacted_object[9], impacted_object[10], impacted_object[11], impacted_object[12], impacted_object[13], impacted_object[14], impacted_object[15], impacted_object[16], impacted_object[17])
+                            self.mycursor.execute(sql,val)
+                            mydb.commit()
+                            f += 1
+                    except:
+                        pass
+                    j+=1
                 i += 1
             #print('Page: ',z)
         
-    def vehicle_journey(self):
+    def vehicle_journey(self): # insert vehicle_journey
         result = extract.Sort().how_many_files(1)
 
         for z in range(result-1):
             i=0
-            data = extract.Sort().which_data(z)
+            data = extract.Sort().which_data(z,1)
             for x in data['vehicle_journeys']:
                 j = 0
                 for y in data['vehicle_journeys'][i]['calendars']:
@@ -120,27 +123,21 @@ class Insert():
                     j += 1
                 i += 1
 
-    def routes(self):
-        result = extract.Sort().how_many_files(1)
+    def routes(self): # insert routes
+        result = extract.Sort().how_many_files(2)
 
-        for z in range(result-1):
+        for z in range(result):
             i=0
-            data = extract.Sort().which_data(z)
-            for x in data['vehicle_journeys']:
-                routes = extract.Sort().routes(z,i,j)
-                for a in range(len(self.five)):
-                    if self.five[a] == 'data':
-                        sql = f"INSERT INTO routes ({self.five[a]}) VALUES (str_to_date('{routes[a]}','%Y-%m-%d'))"
-                    elif disruption[a] == 'NULL':
-                            sql = f"INSERT INTO disruptions ({self.five[a]}) VALUES ({routes[a]})"
-                    else:
-                        sql = f'INSERT INTO routes ({self.five[a]}) VALUES ("{routes[a]}")'
-                    self.mycursor.execute(sql)
-                    mydb.commit()
+            data = extract.Sort().which_data(z,2)
+            for x in data['routes']:
+                routes = extract.Sort().routes(z,i)
+                sql = "INSERT INTO routes (date, embedded_type, id_direction, name_direction, quality, value, lat, lon, id_stop_area, label_stop_area, name_stop_area, direction_type, id, is_frequence, closing_time, id_commercial_mode, name_commercial_mode, id_line, id_name, opening_time, id_physical_modes, name_physical_modes, routes_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"             
+                val = (routes[0], routes[1], routes[2], routes[3], routes[4], routes[5], routes[6], routes[7], routes[8], routes[9], routes[10], routes[11], routes[12], routes[13], routes[14], routes[15], routes[16], routes[17], routes[18], routes[19], routes[20], routes[21], routes[22])
+                self.mycursor.execute(sql,val)
+                mydb.commit()
                 i += 1
 
-
-#Create().table()
-Insert().disruptions()
+Create().table()
+#Insert().disruptions()
 #Insert().vehicle_journey()
-#Insert().routes()
+Insert().routes()
